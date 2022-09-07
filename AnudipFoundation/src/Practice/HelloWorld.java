fa9d6b1ec9bf19f6c6495d70f06e2628f5a52b7a
LENOVO IP3 7HIN <LENOVO IP3 7HIN@Akshay-Aute>
2022-09-07 12:38:36 +0530

    Second Commit

--- AnudipFoundation/src/Practice/HelloWorld.java
+++ AnudipFoundation/src/Practice/HelloWorld.java
@@ -3,6 +3,7 @@ package Practice;
 public class HelloWorld{
 
 	public static void main(String[] args) {
+		
 		int n = 2;
 		System.out.println("Table of "+ n +" is ; ");
 		for(int i = 1; i<=10;i++) {
