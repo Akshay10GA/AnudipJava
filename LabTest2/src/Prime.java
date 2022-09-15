2444983c07b8c49b4ecffa369070db605dade176
LENOVO IP3 7HIN <LENOVO IP3 7HIN@Akshay-Aute>
2022-09-15 17:48:02 +0530

    LabTest2 Commit

--- LabTest2/src/Prime.java
+++ LabTest2/src/Prime.java
@@ -3,7 +3,7 @@ public class Prime {
     public static void PrimeNumber(int n){
         boolean prime = false;
         if(n>2){
-            System.out.print("Prime numbers from 1 to 20 are : 1 2 ");
+            System.out.print("Prime numbers from 1 to 20 are : 2 ");
             for(int i = 2;i<=20;i++){
                 for(int j=2;j<(i);j++){
                     if(i%j==0){
